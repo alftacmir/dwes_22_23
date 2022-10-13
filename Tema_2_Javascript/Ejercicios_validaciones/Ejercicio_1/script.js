28e7dba40d3e6bafed82a21a536c92537e77831b
alftacmir <alum.ataconm.2021@iesalixar.org>
2022-10-13 17:13:44 +0200

    Ejercicio 2 terminado

--- Tema_2_Javascript/Ejercicios_validaciones/Ejercicio_1/script.js
+++ Tema_2_Javascript/Ejercicios_validaciones/Ejercicio_1/script.js
@@ -1,27 +1,71 @@
-function enviar(){
-    const $FORM = document.getElementById("formulario")
-    const $DNI = document.getElementById("dni")
-    const $NOMBRE = document.getElementById("nombre")
-    const $APELLIDOS = document.getElementById("apellidos")
-    const $TELEFONO = document.getElementById("telefono")
-    const $COMENTARIO = document.getElementById("comentario")
-    const $HORA = document.getElementById("hora")
+const $FORM = document.getElementById("formulario");
+const $DNI = document.getElementById("dni");
+const $NOMBRE = document.getElementById("nombre");
+const $APELLIDOS = document.getElementById("apellidos");
+const $TELEFONO = document.getElementById("telefono");
+const $COMENTARIO = document.getElementById("comentario");
+const $HORA = document.getElementById("hora");
 
-    function validDNI(dni){
-        const validacion = /^[0-9]{2}[.][0-9]{3}[.][0-9]{3}[-][A-Z]$/
-        return validacion.test(dni)
+function handleSubmit(e) {
+  e.preventDefault();
+  const dni = $DNI.value;
+  const nombre = $NOMBRE.value;
+  const apellidos = $APELLIDOS.value;
+  const telefono = $TELEFONO.value;
+  const comentario = $COMENTARIO.value;
+  const hora = $HORA.value;
+  if (dni == false || validDNI(dni) == false) {
+    alert(
+      "El DNI debe ser un valor válido : \n -Debe estar relleno \n - Debe tener este formato XX.XXX.XXX-L"
+    );
+  } else {
+    if (nombre == false || validNombre(nombre) == false) {
+      alert(
+        "El nombre debe ser un valor válido : \n -Debe estar relleno \n - Solo tener uno o dos nombres"
+      );
+    } else {
+      if (apellidos == false || validApellidos(apellidos) == false) {
+        alert(
+          "El apellido tiene que tener un formato válido : \n -Debe estar relleno \n - Solo tener uno o dos apellidos"
+        );
+      } else {
+        if (telefono == false || validTelefono(telefono) == false) {
+          alert(
+            "El telefono tiene que tener un formato válido : \n -Debe estar relleno \n - Debe tener nueve numeros y el +34 primero"
+          );
+        } else {
+          let regUsuario = {
+            dni: dni,
+            nombre: nombre,
+            apellidos: apellidos,
+            telefono: telefono,
+            comentario: comentario,
+            hora: hora,
+          };
+          let newUsuario = JSON.stringify(regUsuario);
+          console.log(newUsuario);
+        }
+      }
     }
-    function validNombre(nombre){
-        const validacion = /^(([a-zA-ZáéíóúñÑÁÉÍÓÚ]+)|([a-zA-ZáéíóúñÑÁÉÍÓÚ]+\s[a-zA-ZáéíóúñÑÁÉÍÓÚ]+))$/
-        return validacion.test(nombre)
-    }
-    function validNombre(apellidos){
-        const validacion = /^(([a-zA-ZáéíóúñÑÁÉÍÓÚ]+)|([a-zA-ZáéíóúñÑÁÉÍÓÚ]+\s[a-zA-ZáéíóúñÑÁÉÍÓÚ]+))$/
-        return validacion.test(apellidos)
-    }
-    function validTelefono(telefono){
-        const validacion = /^[+][3][4]+\s[0-9]{9}$/
-        return validacion.test(telefono)
-    }
-
+  }
 }
+
+function validDNI(dni) {
+  const validacion = /^[0-9]{2}[.][0-9]{3}[.][0-9]{3}[-][A-Z]$/;
+  return validacion.test(dni);
+}
+function validNombre(nombre) {
+  const validacion =
+    /^(([a-zA-ZáéíóúñÑÁÉÍÓÚ]+)|([a-zA-ZáéíóúñÑÁÉÍÓÚ]+\s[a-zA-ZáéíóúñÑÁÉÍÓÚ]+))$/;
+  return validacion.test(nombre);
+}
+function validApellidos(apellidos) {
+  const validacion =
+    /^(([a-zA-ZáéíóúñÑÁÉÍÓÚ]+)|([a-zA-ZáéíóúñÑÁÉÍÓÚ]+\s[a-zA-ZáéíóúñÑÁÉÍÓÚ]+))$/;
+  return validacion.test(apellidos);
+}
+function validTelefono(telefono) {
+  const validacion = /^[+][3][4]+\s[0-9]{9}$/;
+  return validacion.test(telefono);
+}
+$FORM.addEventListener("submit", handleSubmit);
---
+++ Tema_2_Javascript/Ejercicios_validaciones/Ejercicio_2/script.js
@@ -0,0 +1,67 @@
+const $FORM = document.getElementById("formulario");
+const $NOMBRE = document.getElementById("nombre");
+const $APELLIDOS = document.getElementById("apellidos");
+const $EMAIL = document.getElementById("email");
+const $WEB = document.getElementById("web");
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const nombre = $NOMBRE.value;
+    const apellidos = $APELLIDOS.value;
+    const email = $EMAIL.value;
+    const web = $WEB.value;
+    if (email == false || validEmail(email) == false) {
+      alert(
+        "El DNI debe ser un valor válido : \n -Debe estar relleno \n - Debe tener el formato de ejemolo"
+      );
+    } else {
+      if (nombre == false || validNombre(nombre) == false) {
+        alert(
+          "El nombre debe ser un valor válido : \n -Debe estar relleno \n - Solo tener uno o dos nombres"
+        );
+      } else {
+        if (apellidos == false || validApellidos(apellidos) == false) {
+          alert(
+            "El apellido tiene que tener un formato válido : \n -Debe estar relleno \n - Solo tener uno o dos apellidos"
+          );
+        } else {
+          if (web == false || validWeb(web) == false) {
+            alert(
+              "La web tiene que tener un formato válido : \n -Debe estar relleno \n - Debe tener formato como el ejemplo"
+            );
+          } else {
+            let regUsuario = {
+              Nombre: nombre,
+              Apellidos: apellidos,
+              Email: email,
+              Web: web,
+            };
+            let newUsuario = JSON.stringify(regUsuario);
+            console.log(newUsuario);
+          }
+        }
+      }
+    }
+  }
+  
+
+function validNombre(nombre) {
+    const validacion =
+      /^(([a-zA-ZáéíóúñÑÁÉÍÓÚ]+)|([a-zA-ZáéíóúñÑÁÉÍÓÚ]+\s[a-zA-ZáéíóúñÑÁÉÍÓÚ]+))$/;
+    return validacion.test(nombre);
+}
+function validApellidos(apellidos) {
+    const validacion =
+      /^(([a-zA-ZáéíóúñÑÁÉÍÓÚ]+)|([a-zA-ZáéíóúñÑÁÉÍÓÚ]+\s[a-zA-ZáéíóúñÑÁÉÍÓÚ]+))$/;
+    return validacion.test(apellidos);
+}
+function validEmail(email){
+    const validacion = /^([a-zA-Z]+)[@]([a-z]+)[.]([a-z]{2,3})$/
+    return validacion.test(email)
+}
+function validWeb(web){
+    const validacion = /^(https:)[/][/](www.)[a-z]+[.][a-z]{2,3}$/
+    return validacion.test(web)
+}
+
+$FORM.addEventListener("submit", handleSubmit);
